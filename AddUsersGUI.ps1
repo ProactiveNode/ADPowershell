4db793adb10f82fb6eaab7017ed4aaa90548a068
Adrian C <adc9978@rit.edu>
2018-11-20 22:55:48 -0500

    Update AddUsersGUI.ps1

--- AddUsersGUI.ps1
+++ AddUsersGUI.ps1
@@ -1,9 +1,20 @@
 #Creates Base Window
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
+
 $Screen = New-Object System.Windows.Forms.Form
 $Screen.Text = "Active Directory"
 $Screen.Size = New-Object System.Drawing.Size(550,400)
+$Global:counter = 30
+
+#Creates the new rows
+$createNewRow = {
+	$Global:counter = $Global:counter + 30
+	$newRow = New-Object System.Windows.Forms.TextBox
+	$newRow.Location = New-Object System.Drawing.Point(10,$counter)
+	$newRow.Size = New-Object System.Drawing.Size(100,$counter)
+	$Screen.Controls.Add($newRow)
+}
 
 #Titles 
 $Column = New-Object System.Windows.Forms.Label
@@ -15,9 +26,14 @@ $Screen.Controls.Add($Column)
 $FirstName = New-Object System.Windows.Forms.TextBox
 $FirstName.Location = New-Object System.Drawing.Point(10,30)
 $FirstName.Size = New-Object System.Drawing.Size(100,30)
-
 $Screen.Controls.Add($FirstName)
 
+$addRowButton = New-Object System.Windows.Forms.Button
+$addRowButton.Location = New-Object System.Drawing.Point(120,10)
+$addRowButton.Size = New-Object System.Drawing.Size(70,20)
+$addRowButton.Text = "Make Row"
+$addRowButton.add_Click($createNewRow)
+$Screen.Controls.Add($addRowButton)
 
 #Displays the Final Window with everything on it
 $Screen.ShowDialog()
